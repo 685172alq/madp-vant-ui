1cd716c188a025648676c55c5b923850769d4c2a
凌权 <1135993902@qq.com>
2022-04-24 13:26:34 +0800

    hh

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import "amfe-flexible"; //能够动态设置rem基准值，即根据手机屏幕
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./utils/vant";
+import "@/assets/style/common.scss";
+// import VConsole from "vconsole";
+import "./utils/madp";
+import { rpc } from "./utils/rpc.js";
 
 Vue.config.productionTip = false;
 
@@ -11,3 +16,8 @@ new Vue({
     store,
     render: (h) => h(App),
 }).$mount("#app");
+Vue.prototype.$rpc = rpc;
+
+window.clientMode = false;
+// 初始化
+// var vConsole = new VConsole();
